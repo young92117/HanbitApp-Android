4400d2ac49f94e0d48c0f3628cdbade380cf5c7f
Jack Yoo <jack.yoo@gmail.com>
2014-03-14 16:51:43 -0700

    Showing news when the news is clicked as a dialog. It might be changed to expandableListView later.
    	new file:   res/layout/news_content.xml
    	new file:   res/layout/news_row.xml
    	modified:   src/main/java/org/sdhanbit/mobile/android/activities/News.java

--- src/main/java/org/sdhanbit/mobile/android/activities/News.java
+++ src/main/java/org/sdhanbit/mobile/android/activities/News.java
@@ -8,11 +8,19 @@ import org.sdhanbit.mobile.android.R;
 import org.sdhanbit.mobile.android.entities.FeedEntry;
 import org.sdhanbit.mobile.android.managers.FeedEntryManager;
 
+import android.app.AlertDialog;
+import android.app.AlertDialog.Builder;
+import android.app.Dialog;
 import android.content.Context;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class News {
 	private static String TAG = "News";
@@ -29,34 +37,68 @@ public class News {
 	public void constructNews()
     {
     	Log.v(TAG, "Starting News");
-    	final ArrayList<String> list = new ArrayList<String>();
    	 	ListView news_list = (ListView)(mainView.findViewById(R.id.news_list));
    	 	List<FeedEntry> entries = feedEntryManager.getFeedEntries("15");
- 		for(int i = 0 ; i < entries.size(); i++)
-    	{
-    		Log.v(TAG, entries.get(i).getTitle());
-    		list.add(entries.get(i).getTitle());
-    	}
-   	    final StableArrayAdapter adapter = new StableArrayAdapter(mContext,
-    	        android.R.layout.simple_list_item_1, list);
+   	    final FeedArrayAdapter adapter = new FeedArrayAdapter(mContext,
+    	        android.R.layout.simple_list_item_1, entries);
     	news_list.setAdapter(adapter);
+    	news_list.setOnItemClickListener(new FeedItemClickListener());
     }
 	
-	class StableArrayAdapter extends ArrayAdapter<String> {
+	class FeedItemClickListener implements AdapterView.OnItemClickListener
+	{
+		public void onItemClick(AdapterView<?> parent, View view, int position, long id)
+		{
+//			View row_view = parent.getAdapter().getView(position, view, parent);
+//			((TextView)(row_view.findViewById(R.id.content)))
+//			    .setText(((FeedEntry)(parent.getAdapter().getItem(position))).getContent());
+			
+			final Dialog dialog = new Dialog(mContext);
+			dialog.setContentView(R.layout.news_content);
+			dialog.setTitle(((FeedEntry)(parent.getAdapter().getItem(position))).getTitle());
+ 
+			TextView text = (TextView) dialog.findViewById(R.id.content);
+			text.setText(((FeedEntry)(parent.getAdapter().getItem(position))).getContent());
+ 
+//			Button dialogButton = (Button) dialog.findViewById(R.id.dialogButtonOK);
+//			// if button is clicked, close the custom dialog
+//			dialogButton.setOnClickListener(new OnClickListener() {
+//				@Override
+//				public void onClick(View v) {
+//					dialog.dismiss();
+//				}
+//			});
+ 
+			dialog.show();
+		}
+	}
+	
+	class FeedArrayAdapter extends ArrayAdapter<FeedEntry> {
 
-	    HashMap<String, Integer> mIdMap = new HashMap<String, Integer>();
+	    HashMap<FeedEntry, Integer> mIdMap = new HashMap<FeedEntry, Integer>();
 
-	    public StableArrayAdapter(Context context, int textViewResourceId,
-	        List<String> objects) {
+	    public FeedArrayAdapter(Context context, int textViewResourceId,
+	        List<FeedEntry> objects) {
 	      super(context, textViewResourceId, objects);
 	      for (int i = 0; i < objects.size(); ++i) {
 	        mIdMap.put(objects.get(i), i);
 	      }
 	    }
+	    
+	    public View getView(int position, View convertView,	ViewGroup parent) 
+	    {
+			LayoutInflater inflater = (LayoutInflater)mContext.getSystemService( Context.LAYOUT_INFLATER_SERVICE );
+			View row=inflater.inflate(R.layout.news_row, parent, false);
+			TextView label=(TextView)row.findViewById(R.id.content);
+			label.setText((CharSequence)(getItem(position)).getContent().subSequence(0, 20)+"...");
+			TextView date=(TextView)row.findViewById(R.id.date);
+			date.setText((CharSequence)(getItem(position)).getDate());
+			return(row);
+	    }
 
 	    @Override
 	    public long getItemId(int position) {
-	      String item = getItem(position);
+	      FeedEntry item = getItem(position);
 	      return mIdMap.get(item);
 	    }
 
