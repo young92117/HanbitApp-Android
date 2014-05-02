2fe936a9d1f682c472d08cead196483ae53aa92a
holybit7 <holybit@gmail.com>
2014-05-01 23:16:49 -0700

    금주사역 수정중

--- src/main/java/org/sdhanbit/mobile/android/activities/Actions.java
+++ src/main/java/org/sdhanbit/mobile/android/activities/Actions.java
@@ -10,25 +10,41 @@ import android.content.Context;
 import android.util.Log;
 import android.view.View;
 import android.widget.ListView;
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebView;
 
 import com.haarman.listviewanimations.swinginadapters.prepared.AlphaInAnimationAdapter;
+import roboguice.activity.event.OnCreateEvent;
 
-public class Actions {
+public class Actions extends Activity {
 
 	private static String TAG = "Actions";
 	private Context mContext;
 	private FeedEntryManager feedEntryManager;
 	private View mainView;
-	
+
+    public Actions(){}
+
 	public Actions(Context context, FeedEntryManager feedEntryManager, View mainView)
 	{
 		mContext = context;
 		this.feedEntryManager = feedEntryManager;
 		this.mainView = mainView;
+
 	}
-	
+
 	public void construct()
     {
     	Log.v(TAG, "Starting Actions");
+
+
     }
+
+    public void onCreate(Bundle savedInstanceState) {
+        WebView browser = (WebView) findViewById(R.id.WebViewAct);
+        setContentView(R.layout.actions);
+        browser.loadUrl("http://www.google.com");
+    }
+
 }
