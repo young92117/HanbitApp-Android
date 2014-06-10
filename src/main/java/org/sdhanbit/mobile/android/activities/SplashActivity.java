c35014d4ba7505cec0ed2ccee8e49212a3af2a1b
Daniel Park <daniel.jy.park@gmail.com>
2014-06-10 01:27:11 -0700

    updated map image implemented "touch to skip" splash screen

--- src/main/java/org/sdhanbit/mobile/android/activities/SplashActivity.java
+++ src/main/java/org/sdhanbit/mobile/android/activities/SplashActivity.java
@@ -5,13 +5,16 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.MotionEvent;
 import android.widget.VideoView;
 import org.sdhanbit.mobile.android.R;
 
 public class SplashActivity extends Activity {
 
     // Splash screen timer
-    private static int SPLASH_TIME_OUT = 2000;
+    private static int SPLASH_TIME_OUT = 5000;
+
+    private boolean alreadyGone = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,15 +33,29 @@ public class SplashActivity extends Activity {
             @Override
             public void run() {
                 // This method will be executed once the timer is over
-                // Start your app main activity
-                Intent i = new Intent(SplashActivity.this, MainActivity.class);
-                startActivity(i);
-                // close this activity
-                finish();
-
-                overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+                closeSplash();
             }
         }, SPLASH_TIME_OUT);
     }
 
+    private void closeSplash() {
+        if(!alreadyGone) {
+            alreadyGone = true;
+            // Start your app main activity
+            Intent i = new Intent(SplashActivity.this, MainActivity.class);
+            startActivity(i);
+            // close this activity
+            finish();
+
+            overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+        }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            closeSplash();
+        }
+        return true;
+    }
 }
